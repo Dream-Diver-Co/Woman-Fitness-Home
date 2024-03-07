8ce77c6461202463ab7003da5c6e85add89ca023
Sakib <sahadothosain6619@gmail.com>
2024-03-07 15:38:01 +0600

    final home page done with responsiv please check

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./gallery.css";
-import image10 from "../../assets/images/product2.webp";
-import image2 from "../../assets/images/product3.webp";
-import image3 from "../../assets/images/product7.webp";
-import image4 from "../../assets/images/product5.webp";
+import image0 from "../../assets/images/Sports/sports3.jpeg";
+import image2 from "../../assets/images/Sports/sports4.jpeg";
+import image3 from "../../assets/images/evant/evant5.jpg";
+import image4 from "../../assets/images/poll6.webp";
 import image5 from "../../assets/images/poll1.webp";
-import image6 from "../../assets/images/product9.webp";
-import image7 from "../../assets/images/product8.webp";
+import image6 from "../../assets/images/evant/evant6.jpg";
+import image7 from "../../assets/images/Sports/sports7.jpeg";
 import image8 from "../../assets/images/poll.jpg";
 import image9 from "../../assets/images/evant/evant3.jpg";
 import image1 from "../../assets/images/evant/evant3.jpg";
@@ -21,26 +21,35 @@ const Gallery = () => {
     image7,
     image8,
     image9,
-    image10,
+    image0,
   ];
   return (
-    <div className="gallery">
-      <div className="column">
-        {image.map((image, index) => (
-          <div className="pics" key={index}>
-            <img
-              className={`image-${index}`}
-              src={image}
-              alt={index}
-              data-aos="flip-left"
-              data-aos-easing="ease-out-cubic"
-              data-aos-duration="2000"
-              style={{ width: "100%", height: "auto" }}
-            />
-          </div>
-        ))}
+    <>
+      <div className="text-center text-dark my-5">
+        <h1>
+          {" "}
+          Few <span className="text-scend fw-bold nuts-f">Moments</span> At
+          ROTTERDAMP
+        </h1>
       </div>
-    </div>
+      <div className="gallery">
+        <div className="column">
+          {image.map((image, index) => (
+            <div className="pics" key={index}>
+              <img
+                className={`image-${index}`}
+                src={image}
+                alt={index}
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                style={{ width: "100%", height: "auto" }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
